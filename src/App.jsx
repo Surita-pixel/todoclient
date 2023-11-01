2c208be07cf0eb181cf93a45f8970673d9467bc8
surita <suritabtc@gmail.com>
2023-11-01 13:03:58 -0500

    corregido error de importación

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import {Toaster} from 'react-hot-toast'
 
-import TaskPage from './pages/taskPage'; "./pages";
+import TaskPage from './pages/TaskPage'; 
 import TaskFormPage from './pages/TaskFormPage';
 import Navigation from './components/Navigation';
 export default function App() {
